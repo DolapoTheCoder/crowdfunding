69620b71faaaefed4b4dcefc8dd1b5424785e743
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-08-10 10:11:36 +0100

    Create Campaign

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -6,10 +6,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod crowdfunding {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        //recieve account from campaign
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        //*ctx.accounts.user.key = msg.sender
+        account.admin = *ctx.accounts.user.key;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
