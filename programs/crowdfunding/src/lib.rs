7e12c697fa24c9ea3a736a14d6bf7dc3d9192fe2
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-08-10 12:18:31 +0100

    deployed

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4GTfnVe1TX6EyTaVYrqX1x3msYbVto4f6TSFywYLDYmn");
 
 #[program]
 pub mod crowdfunding {
