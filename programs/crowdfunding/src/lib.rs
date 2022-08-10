c11ef40f4964880a54afee1b44e749dc80309017
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-08-10 11:47:16 +0100

    Withdraw Function + Struct

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,7 +7,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod crowdfunding {
     use super::*;
 
-    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> ProgramResult {
         //recieve account from campaign
         let campaign = &mut ctx.accounts.campaign;
         campaign.name = name;
@@ -14,6 +15,24 @@ pub mod crowdfunding {
         campaign.amount_donated = 0;
         //*ctx.accounts.user.key = msg.sender
         campaign.admin = *ctx.accounts.user.key;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+        if campaign.admin != *user.key{
+            //not authorised
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+        //CHECK IF THE CAMPAIGN HAS ENOUGH TO WITHDRAW
+        if **campaign.to_account_info().lamports.borrow() - rent_balance < amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+        //how to transfer funds from one wallet to another in rust
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
         Ok(())
     }
 }
@@ -28,6 +47,14 @@ pub struct Create<'info> {
     pub system_program: Program<'info, System>
 }
 
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
 #[account]
 pub struct Campaign {
     pub admin: Pubkey,
@@ -35,3 +62,4 @@ pub struct Campaign {
     pub description: String,
     pub amount_donated: u64
 }
+
